cd1bdc51ff35f9d27bbb839d4b3578b530f3f9a3
Anshul Goyal <anshulgoel151999@gmail.com>
2020-05-01 13:51:53 +0530

    Fixed async code

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,12 +1,15 @@
 use nodejs_sys::{
-    napi_adjust_external_memory, napi_async_work, napi_callback_info, napi_create_async_work,
-    napi_create_error, napi_create_external_arraybuffer, napi_create_function, napi_create_promise,
-    napi_create_string_utf8, napi_deferred, napi_define_class, napi_delete_async_work, napi_env,
-    napi_get_arraybuffer_info, napi_get_cb_info, napi_get_undefined, napi_get_value_int32,
-    napi_get_value_string_utf8, napi_property_attributes, napi_property_descriptor,
-    napi_queue_async_work, napi_ref, napi_reject_deferred, napi_resolve_deferred,
-    napi_set_property, napi_status, napi_unwrap, napi_value, napi_wrap,
+    napi_adjust_external_memory, napi_call_threadsafe_function, napi_callback_info,
+    napi_create_external_arraybuffer, napi_create_function, napi_create_promise,
+    napi_create_string_utf8, napi_create_threadsafe_function, napi_deferred, napi_define_class,
+    napi_env, napi_get_arraybuffer_info, napi_get_cb_info, napi_get_undefined,
+    napi_get_value_int32, napi_get_value_string_utf8, napi_property_attributes,
+    napi_property_descriptor, napi_reject_deferred, napi_release_threadsafe_function,
+    napi_resolve_deferred, napi_set_property, napi_status, napi_threadsafe_function,
+    napi_threadsafe_function_call_mode, napi_threadsafe_function_release_mode, napi_unwrap,
+    napi_value, napi_wrap,
 };
+
 use std::ffi::{c_void, CString};
 
 pub unsafe fn get_string(env: napi_env, val: napi_value) -> String {
@@ -134,6 +137,7 @@ pub unsafe extern "C" fn get_output_buffer_bytes(
     let mut val = std::mem::zeroed();
     napi_unwrap(env, js_this, &mut val);
     let task: Box<Context> = Box::from_raw(std::mem::transmute(val));
+    // println!("{:?}", task);
     let mut read = 0 as i32;
     napi_get_value_int32(env, args_buffer[0], &mut read);
     match &task.inner {
@@ -144,12 +148,12 @@ pub unsafe extern "C" fn get_output_buffer_bytes(
                     napi_create_external_arraybuffer(
                         env,
                         data.as_ptr() as *mut c_void,
-                        v.len(),
+                        data.len(),
                         Some(handle_buffer_drop),
-                        Box::into_raw(Box::new(v.len())) as *mut c_void,
+                        Box::into_raw(Box::new(data.len())) as *mut c_void,
                         &mut local,
                     );
-                    napi_adjust_external_memory(env, v.len() as i64, std::ptr::null_mut());
+                    napi_adjust_external_memory(env, data.len() as i64, std::ptr::null_mut());
                     std::mem::forget(data);
                 }
                 Err(err) => {
@@ -162,74 +166,11 @@ pub unsafe extern "C" fn get_output_buffer_bytes(
     Box::into_raw(task);
     local
 }
-
-struct MessageTask {
-    message: String,
-    method: String,
-    result: Option<Result<String, String>>,
-    context: Box<imageflow_core::Context>,
-    work: napi_async_work,
-    deferred: napi_deferred,
-}
-
-pub unsafe extern "C" fn perform_task(_env: napi_env, data: *mut c_void) {
-    let mut task: Box<MessageTask> = Box::from_raw(std::mem::transmute(data));
-    let (response, _result) = task.context.message(&task.method, task.message.as_bytes());
-    match std::str::from_utf8(&response.response_json.to_vec()) {
-        Ok(string_response) => {
-            task.result = Some(Ok(string_response.to_owned()));
-        }
-        Err(err) => task.result = Some(Err(err.to_string())),
-    };
-    Box::into_raw(task);
-}
-
-pub unsafe extern "C" fn complete_task(env: napi_env, _status: napi_status, data: *mut c_void) {
-    let task: Box<MessageTask> = Box::from_raw(std::mem::transmute(data));
-    let v = match task.result {
-        Some(d) => match d {
-            Ok(result) => result,
-            Err(err) => {
-                let mut js_error: napi_value = std::mem::zeroed();
-                let mut error_code: napi_value = std::mem::zeroed();
-                let mut error_string = std::mem::zeroed();
-                create_string(env, "100", &mut error_code);
-                create_string(env, &err, &mut error_string);
-                napi_create_error(env, error_code, error_string, &mut js_error);
-                napi_reject_deferred(env, task.deferred, js_error);
-                napi_delete_async_work(env, task.work);
-                return;
-            }
-        },
-        None => {
-            let mut js_error: napi_value = std::mem::zeroed();
-            let mut error_code: napi_value = std::mem::zeroed();
-            let mut error_string = std::mem::zeroed();
-            create_string(env, "100", &mut error_code);
-            create_string(env, "Error while performing task", &mut error_string);
-            napi_create_error(env, error_code, error_string, &mut js_error);
-            napi_reject_deferred(env, task.deferred, js_error);
-            napi_delete_async_work(env, task.work);
-            return;
-        }
-    };
-    let mut response: napi_value = std::mem::zeroed();
-    create_string(env, &v, &mut response);
-    napi_resolve_deferred(env, task.deferred, response);
-    napi_delete_async_work(env, task.work);
-}
-
-pub unsafe extern "C" fn message(env: napi_env, info: napi_callback_info) -> napi_value {
+pub unsafe extern "C" fn message_sync(env: napi_env, info: napi_callback_info) -> napi_value {
     let mut js_this: napi_value = std::mem::zeroed();
     let mut s = 2;
     let mut args_buffer: [napi_value; 2] = std::mem::zeroed();
     let args_ptr = args_buffer.as_mut_ptr();
-    let mut promise: napi_value = std::mem::zeroed();
-    let mut deferred: napi_deferred = std::mem::zeroed();
-    let mut work_name: napi_value = std::mem::zeroed();
-    let mut work: napi_async_work = std::mem::zeroed();
-    create_string(env, "Message Function", &mut work_name);
-    napi_create_promise(env, &mut deferred, &mut promise);
     napi_get_cb_info(
         env,
         info,
@@ -243,32 +184,103 @@ pub unsafe extern "C" fn message(env: napi_env, info: napi_callback_info) -> nap
     let mut task: Box<Context> = Box::from_raw(std::mem::transmute(val));
     let method = get_string(env, args_buffer[0]);
     let message = get_string(env, args_buffer[1]);
-    let context = match task.inner {
-        Some(ctx) => ctx,
-        None => panic!("error"),
+    let mut inner = task.inner.expect("Context");
+    let (response, _result) = inner.message(&method, message.as_bytes());
+    let re = match std::str::from_utf8(&response.response_json.to_vec()) {
+        Ok(string_response) => string_response.to_owned(),
+        Err(err) => err.to_string(),
     };
-    task.inner = None;
+    let mut s: napi_value = std::mem::zeroed();
+    create_string(env, &re, &mut s);
+    task.inner = Some(inner);
     std::mem::forget(task);
-    let message_task = MessageTask {
-        message,
-        method,
-        context,
-        result: None,
-        work,
-        deferred,
+    s
+}
+
+struct ThreadSafeFunction(napi_threadsafe_function);
+unsafe impl Send for Context {}
+unsafe impl Send for ThreadSafeFunction {}
+
+pub unsafe extern "C" fn task_compelete(
+    env: napi_env,
+    _js_cb: napi_value,
+    ctx: *mut c_void,
+    data: *mut c_void,
+) {
+    let value: Box<Result<String, String>> =
+        Box::from_raw(std::mem::transmute(Box::from_raw(data)));
+    match value.as_ref() {
+        Ok(s) => {
+            let mut result: napi_value = std::mem::zeroed();
+            create_string(env, &s, &mut result);
+            napi_resolve_deferred(env, ctx as napi_deferred, result);
+        }
+        Err(e) => {
+            let mut result: napi_value = std::mem::zeroed();
+            create_string(env, &e, &mut result);
+            napi_reject_deferred(env, ctx as napi_deferred, result);
+        }
     };
-    let raw = Box::into_raw(Box::new(message_task));
-    napi_create_async_work(
+}
+
+pub unsafe extern "C" fn message(env: napi_env, info: napi_callback_info) -> napi_value {
+    let mut js_this: napi_value = std::mem::zeroed();
+    let mut s = 2;
+    let mut args_buffer: [napi_value; 2] = std::mem::zeroed();
+    let args_ptr = args_buffer.as_mut_ptr();
+    let mut promise: napi_value = std::mem::zeroed();
+    let mut deferred: napi_deferred = std::mem::zeroed();
+    let mut work_name: napi_value = std::mem::zeroed();
+    create_string(env, "Message Function", &mut work_name);
+    napi_create_promise(env, &mut deferred, &mut promise);
+    napi_get_cb_info(
+        env,
+        info,
+        &mut s,
+        args_ptr,
+        &mut js_this,
+        std::ptr::null_mut(),
+    );
+    let mut val = std::mem::zeroed();
+    napi_unwrap(env, js_this, &mut val);
+    let method = get_string(env, args_buffer[0]);
+    let message = get_string(env, args_buffer[1]);
+    let mut task: Box<Context> = Box::from_raw(std::mem::transmute(val));
+    let mut tsfn: napi_threadsafe_function = std::ptr::null_mut();
+    napi_create_threadsafe_function(
         env,
         std::ptr::null_mut(),
+        std::ptr::null_mut(),
         work_name,
-        Some(perform_task),
-        Some(complete_task),
-        std::mem::transmute(raw),
-        &mut work,
+        0,
+        1,
+        std::ptr::null_mut(),
+        None,
+        deferred as *mut c_void,
+        Some(task_compelete),
+        &mut tsfn,
     );
-    napi_queue_async_work(env, work);
-    (*raw).work = work;
+    let thread_safe = ThreadSafeFunction(tsfn);
+    std::thread::spawn(move || {
+        let mut inner = task.inner.expect("error");
+        let (response, _result) = inner.message(&method, message.as_bytes());
+        let v = response.response_json.to_vec();
+        let value = match std::str::from_utf8(&v) {
+            Ok(v) => Ok(v.to_string()),
+            Err(e) => Err(e.to_string()),
+        };
+        napi_call_threadsafe_function(
+            thread_safe.0,
+            Box::into_raw(Box::new(value)) as *mut c_void,
+            napi_threadsafe_function_call_mode::napi_tsfn_blocking,
+        );
+        napi_release_threadsafe_function(
+            thread_safe.0,
+            napi_threadsafe_function_release_mode::napi_tsfn_release,
+        );
+        task.inner = Some(inner);
+        std::mem::forget(task);
+    });
     promise
 }
 
@@ -367,6 +379,7 @@ pub unsafe extern "C" fn napi_register_module_v1(
     let message_string = CString::new("message").expect("CString::new failed");
 
     let clean_string = CString::new("clean").expect("CString::new failed");
+    let message_sync_string = CString::new("messageSync").expect("CString::new failed");
     let mut properties = [
         napi_property_descriptor {
             utf8name: add_input_bytes_copied_string.as_ptr(),
@@ -418,6 +431,16 @@ pub unsafe extern "C" fn napi_register_module_v1(
             attributes: napi_property_attributes::napi_default,
             data: std::ptr::null_mut(),
         },
+        napi_property_descriptor {
+            utf8name: message_sync_string.as_ptr(),
+            name: std::ptr::null_mut(),
+            method: Some(message_sync),
+            getter: None,
+            setter: None,
+            value: std::ptr::null_mut(),
+            attributes: napi_property_attributes::napi_default,
+            data: std::ptr::null_mut(),
+        },
     ];
     let properties_prt = properties.as_mut_ptr();
     napi_define_class(
@@ -426,7 +449,7 @@ pub unsafe extern "C" fn napi_register_module_v1(
         3,
         Some(create_class),
         std::ptr::null_mut(),
-        5,
+        6,
         properties_prt,
         &mut local,
     );
--- test/index.spec.js
+++ test/index.spec.js
@@ -24,7 +24,7 @@ describe('basic', () => {
 
     job.addInputBytesCopied(0, slice);
 
-    let response = await job.message("v0.1/get_image_info", JSON.stringify({ io_id: 0 }));
+    let response = job.messageSync("v0.1/get_image_info", JSON.stringify({ io_id: 0 }));
     console.log(response);
     let result = JSON.parse(response);
     expect(result.success).toEqual(true);
@@ -32,49 +32,50 @@ describe('basic', () => {
     expect(result.data.image_info.image_width).toEqual(1);
   });
 
-   it('should allow image to be upscaled', async () => {
-     let job = new addon.Job()
-     let array = new Uint8Array(
-       [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52, 0x00,
-         0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x08, 0x06, 0x00, 0x00, 0x00, 0x1F, 0x15, 0xC4, 0x89, 0x00,
-         0x00, 0x00, 0x0A, 0x49, 0x44, 0x41, 0x54, 0x78, 0x9C, 0x63, 0x00, 0x01, 0x00, 0x00, 0x05, 0x00, 0x01,
-         0x0D, 0x0A, 0x2D, 0xB4, 0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4E, 0x44, 0xAE, 0x42, 0x60, 0x82]);
+  it('should allow image to be upscaled', async () => {
+    let job = new addon.Job()
+    let array = new Uint8Array(
+      [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52, 0x00,
+        0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x08, 0x06, 0x00, 0x00, 0x00, 0x1F, 0x15, 0xC4, 0x89, 0x00,
+        0x00, 0x00, 0x0A, 0x49, 0x44, 0x41, 0x54, 0x78, 0x9C, 0x63, 0x00, 0x01, 0x00, 0x00, 0x05, 0x00, 0x01,
+        0x0D, 0x0A, 0x2D, 0xB4, 0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4E, 0x44, 0xAE, 0x42, 0x60, 0x82]);
 
-     let slice = array.buffer.slice(array.byteOffset, array.byteLength + array.byteOffset)
-     job.addInputBytesCopied(0, slice);
-     job.addOutputBuffer(1);
+    let slice = array.buffer.slice(array.byteOffset, array.byteLength + array.byteOffset)
+    job.addInputBytesCopied(0, slice);
+    job.addOutputBuffer(1);
 
-     let job_task = {
-            "framewise": {
-              "steps": [{
-                 "command_string":  {
-                      "kind": "ir4",
-                      "value": "width=100&height=100&scale=both&format=jpg",
-                      "decode": 0,
-                      "encode": 1
-                 }
-               }]
-             }
-         };
+    let job_task = {
+      "framewise": {
+        "steps": [{
+          "command_string": {
+            "kind": "ir4",
+            "value": "width=100&height=100&scale=both&format=jpg",
+            "decode": 0,
+            "encode": 1
+          }
+        }]
+      }
+    };
 
 
 
-     let response = await job.message("v0.1/execute", JSON.stringify(job_task))
-     let result = JSON.parse(response);
-     console.log(result);
-     expect(result.success).toEqual(true);
-     expect(result.code).toEqual(200);
-     expect(result.data.job_result.encodes.length).toEqual(1);
-     expect(result.data.job_result.encodes[0].w).toEqual(100);
-     expect(result.data.job_result.encodes[0].h).toEqual(100);
+    let response = await job.message("v0.1/execute", JSON.stringify(job_task))
+    let result = JSON.parse(response);
+    console.log(result);
+    expect(result.success).toEqual(true);
+    expect(result.code).toEqual(200);
+    expect(result.data.job_result.encodes.length).toEqual(1);
+    expect(result.data.job_result.encodes[0].w).toEqual(100);
+    expect(result.data.job_result.encodes[0].h).toEqual(100);
 
-     let result_bytes = job.getOutputBufferBytes(1);
-     expect(result_bytes.length).toBeGreaterThan(100);
-     expect(result_bytes[0]).toEqual(0xFF);
-     expect(result_bytes[1]).toEqual(0xD8);
-     expect(result_bytes[2]).toEqual(0xFF);
+    let result_bytes = job.getOutputBufferBytes(1);
+    console.log(result_bytes);
+    expect(result_bytes.length).toBeGreaterThan(100);
+    expect(result_bytes[0]).toEqual(0xFF);
+    expect(result_bytes[1]).toEqual(0xD8);
+    expect(result_bytes[2]).toEqual(0xFF);
 
-   });
+  });
 
 
 
