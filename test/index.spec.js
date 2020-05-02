e688068195490b2e33422318befdc4faed069f4f
Anshul Goyal <32068075+anshulrgoyal@users.noreply.github.com>
2020-05-03 05:15:35 +0530

    This pr will migrate the project to NAPI (#2)
    * intial graound work
    
    * Added Array buffer drop code
    
    * Added rest of functionality
    
    * Added long function
    
    * Fixed test
    
    * Fixed test
    
    * Removed weak ref
    
    * Add image execution test
    
    * Fixed async code
    
    * Fixed test
    
    * Remove unsued crates from Cargo.toml
    
    * Make yarn build and yarn build-release work
    
    * Added Error Handling
    
    * Added more error handling
    
    Co-authored-by: Lilith River <lilith@imazen.io>

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,57 +6,86 @@ describe('basic', () => {
     expect(typeof addon.getLongVersionString()).toEqual('string');
   });
 
-  it('should create a context', () => {
-    let job = new addon.JobContext()
+  it('should create a context', async () => {
+    let job = new addon.Job()
 
     expect(typeof job).toEqual('object');
   });
 
-  it('should allow input bytes to be added and image width to be queried', () => {
-    let job = new addon.JobContext()
+  it('should allow input bytes to be added and image width to be queried', async () => {
+    let job = new addon.Job()
     let array = new Uint8Array(
       [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52, 0x00,
-       0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x08, 0x06, 0x00, 0x00, 0x00, 0x1F, 0x15, 0xC4, 0x89, 0x00,
-       0x00, 0x00, 0x0A, 0x49, 0x44, 0x41, 0x54, 0x78, 0x9C, 0x63, 0x00, 0x01, 0x00, 0x00, 0x05, 0x00, 0x01,
-       0x0D, 0x0A, 0x2D, 0xB4, 0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4E, 0x44, 0xAE, 0x42, 0x60, 0x82 ]);
+        0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x08, 0x06, 0x00, 0x00, 0x00, 0x1F, 0x15, 0xC4, 0x89, 0x00,
+        0x00, 0x00, 0x0A, 0x49, 0x44, 0x41, 0x54, 0x78, 0x9C, 0x63, 0x00, 0x01, 0x00, 0x00, 0x05, 0x00, 0x01,
+        0x0D, 0x0A, 0x2D, 0xB4, 0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4E, 0x44, 0xAE, 0x42, 0x60, 0x82]);
 
     let slice = array.buffer.slice(array.byteOffset, array.byteLength + array.byteOffset)
 
     job.addInputBytesCopied(0, slice);
 
-    let result = JSON.parse(job.message("v0.1/get_image_info", JSON.stringify({io_id: 0})));
+    let response = job.messageSync("v0.1/get_image_info", JSON.stringify({ io_id: 0 }));
+    console.log(response);
+    let result = JSON.parse(response);
     expect(result.success).toEqual(true);
     expect(result.code).toEqual(200);
     expect(result.data.image_info.image_width).toEqual(1);
   });
 
-//  it('should allow image to be upscaled', () => {
-//    let job = new addon.JobContext()
-//    let array = new Uint8Array(
-//      [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52, 0x00,
-//       0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x08, 0x06, 0x00, 0x00, 0x00, 0x1F, 0x15, 0xC4, 0x89, 0x00,
-//       0x00, 0x00, 0x0A, 0x49, 0x44, 0x41, 0x54, 0x78, 0x9C, 0x63, 0x00, 0x01, 0x00, 0x00, 0x05, 0x00, 0x01,
-//       0x0D, 0x0A, 0x2D, 0xB4, 0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4E, 0x44, 0xAE, 0x42, 0x60, 0x82 ]);
-//
-//    let slice = array.buffer.slice(array.byteOffset, array.byteLength + array.byteOffset)
-//    job.addInputBytesCopied(0, slice);
-//    job.addOutputBuffer(1);
-//
-//    let result = JSON.parse(job.message("v0.1/execute", JSON.stringify({io_id: 0})));
-//    expect(result.success).toEqual(true);
-//    expect(result.code).toEqual(200);
-//    expect(result.data.image_info.image_width).toEqual(1);
-//  });
+  it('should allow image to be upscaled', async () => {
+    let job = new addon.Job()
+    let array = new Uint8Array(
+      [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A, 0x00, 0x00, 0x00, 0x0D, 0x49, 0x48, 0x44, 0x52, 0x00,
+        0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x01, 0x08, 0x06, 0x00, 0x00, 0x00, 0x1F, 0x15, 0xC4, 0x89, 0x00,
+        0x00, 0x00, 0x0A, 0x49, 0x44, 0x41, 0x54, 0x78, 0x9C, 0x63, 0x00, 0x01, 0x00, 0x00, 0x05, 0x00, 0x01,
+        0x0D, 0x0A, 0x2D, 0xB4, 0x00, 0x00, 0x00, 0x00, 0x49, 0x45, 0x4E, 0x44, 0xAE, 0x42, 0x60, 0x82]);
+
+    let slice = array.buffer.slice(array.byteOffset, array.byteLength + array.byteOffset)
+    job.addInputBytesCopied(0, slice);
+    job.addOutputBuffer(1);
+
+    let job_task = {
+      "framewise": {
+        "steps": [{
+          "command_string": {
+            "kind": "ir4",
+            "value": "width=100&height=100&scale=both&format=jpg",
+            "decode": 0,
+            "encode": 1
+          }
+        }]
+      }
+    };
 
 
 
-//  it('should get dir size', () => {
-//    expect(typeof addon.dirSize(__dirname)).toEqual('string');
-//  });
-//
-//  it('should fail if dir not given', () => {
-//    expect(() => {
-//      addon.dirSize();
-//    }).toThrow();
-//  });
+    let response = await job.message("v0.1/execute", JSON.stringify(job_task))
+    let result = JSON.parse(response);
+    console.log(result);
+    expect(result.success).toEqual(true);
+    expect(result.code).toEqual(200);
+    expect(result.data.job_result.encodes.length).toEqual(1);
+    expect(result.data.job_result.encodes[0].w).toEqual(100);
+    expect(result.data.job_result.encodes[0].h).toEqual(100);
+
+    let result_bytes = job.getOutputBufferBytes(1);
+    result = new Uint8Array(result_bytes);
+    expect(result_bytes.byteLength).toBeGreaterThan(100);
+    expect(result[0]).toEqual(0xFF);
+    expect(result[1]).toEqual(0xD8);
+    expect(result[2]).toEqual(0xFF);
+
+  });
+
+
+
+  //  it('should get dir size', () => {
+  //    expect(typeof addon.dirSize(__dirname)).toEqual('string');
+  //  });
+  //
+  //  it('should fail if dir not given', () => {
+  //    expect(() => {
+  //      addon.dirSize();
+  //    }).toThrow();
+  //  });
 });
