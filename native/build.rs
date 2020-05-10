94e98716ad9c38b4f8cf28b11acf0dc16ddf408b
Anshul Goyal <anshulgoel151999@gmail.com>
2020-05-10 10:07:51 +0530

    Fixed macos build

--- native/build.rs
+++ native/build.rs
@@ -41,7 +41,7 @@ fn main() {
 
 #[cfg(target_os = "macos")]
 fn main() {
-    //println!("cargo:rustc-cdylib-link-arg=-undefined");
+    println!("cargo:rustc-cdylib-link-arg=-undefined");
     println!("cargo:rustc-cdylib-link-arg=dynamic_lookup");
 }
 
